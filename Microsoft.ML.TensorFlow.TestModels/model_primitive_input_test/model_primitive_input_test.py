bcd8cc4910f3d2e029cdf6959eac6e7a3fd270e0
Frank Dong <frdong@microsoft.com>
2020-10-31 04:33:40 +0000

    add test model of use primitive type as input

---
+++ Microsoft.ML.TensorFlow.TestModels/model_primitive_input_test/model_primitive_input_test.py
@@ -0,0 +1,14 @@
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
+input1 = tf.placeholder(tf.string, shape=(None), name="input1")
+input2 = tf.placeholder(tf.string, shape=(None), name="input2")
+
+string_same = tf.cond(tf.equal(input1, input2), lambda: tf.constant(True), lambda: tf.constant(False))
+
+with tf.Session() as sess:
+    string_same = sess.run(string_same, feed_dict={input1: "This is Text1", input2: "This is Text1"})
+    print (string_same)
+    
+    tf.saved_model.simple_save(sess, r'model_primitive_input_test', inputs=inputs, outputs=outputs )
