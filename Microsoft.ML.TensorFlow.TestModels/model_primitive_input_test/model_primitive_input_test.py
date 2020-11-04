3b3dfd7386b6f0cbc5ff4e92878adbe0e9d0e32a
Frank Dong <frdong@microsoft.com>
2020-11-04 04:44:10 +0000

    update tests

--- Microsoft.ML.TensorFlow.TestModels/model_primitive_input_test/model_primitive_input_test.py
+++ Microsoft.ML.TensorFlow.TestModels/model_primitive_input_test/model_primitive_input_test.py
@@ -2,17 +2,17 @@ import tensorflow.compat.v1 as tf
 
 tf.disable_v2_behavior()
 
-input1 = tf.placeholder(tf.string, shape=(None), name="input1")
-input2 = tf.placeholder(tf.string, shape=(None), name="input2")
+input1 = tf.placeholder(tf.string, name="input1")
+input2 = tf.placeholder(tf.string, name="input2")
 
 inputs = {'input1': input1, 'input2': input2}
 
-string_same = tf.cond(tf.equal(input1, input2), lambda: tf.constant(True), lambda: tf.constant(False))
+string_merge = tf.add(input1, input2, name="string_merge")
 
-outputs = {'string_same':string_same}
+outputs = {'string_merge':string_merge}
 
 with tf.Session() as sess:
-    string_same = sess.run(string_same, feed_dict={input1: "This is Text1", input2: "This is Text1"})
-    print (string_same)
-    
-    tf.saved_model.simple_save(sess, r'model_primitive_input_test2', inputs=inputs, outputs=outputs )
+    string_merge = sess.run(string_merge, feed_dict={input1: "This is Text1", input2: "This is Text2"})
+    print (string_merge)
+
+    tf.saved_model.simple_save(sess, r'model_primitive_input_test', inputs=inputs, outputs=outputs )
