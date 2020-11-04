034206c1668fda20477ef5448f64c78adf3db828
Frank Dong <frdong@microsoft.com>
2020-11-04 01:51:15 +0000

    fix for tensorflow output

--- Microsoft.ML.TensorFlow.TestModels/model_primitive_input_test/model_primitive_input_test.py
+++ Microsoft.ML.TensorFlow.TestModels/model_primitive_input_test/model_primitive_input_test.py
@@ -5,10 +5,14 @@ tf.disable_v2_behavior()
 input1 = tf.placeholder(tf.string, shape=(None), name="input1")
 input2 = tf.placeholder(tf.string, shape=(None), name="input2")
 
+inputs = {'input1': input1, 'input2': input2}
+
 string_same = tf.cond(tf.equal(input1, input2), lambda: tf.constant(True), lambda: tf.constant(False))
+
+outputs = {'string_same':string_same}
 
 with tf.Session() as sess:
     string_same = sess.run(string_same, feed_dict={input1: "This is Text1", input2: "This is Text1"})
     print (string_same)
     
-    tf.saved_model.simple_save(sess, r'model_primitive_input_test', inputs=inputs, outputs=outputs )
+    tf.saved_model.simple_save(sess, r'model_primitive_input_test2', inputs=inputs, outputs=outputs )
